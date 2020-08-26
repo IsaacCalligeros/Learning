8c13c407e65a23b399e2ae6446b3c6272ba017af
Isaac Calligeros <isaac.calligeros@gmail.com>
2020-08-26 12:40:19 +0930

    First time playing around with react just following along with https://www.youtube.com/watch?v=sBws8MSXN7A

---
+++ traversymedia-react-tut/src/components/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <React.Fragment>
+            <h1>This is the about page</h1>
+        </React.Fragment>
+    )
+}
